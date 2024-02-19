543212ee0c484cd9b7d58781762ade5b1f78699e
QobilovSardor <note47297@gmail.com>
2024-02-20 00:47:31 +0500

    services section finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,12 @@
 window.addEventListener('DOMContentLoaded', () => {
-	
+	var swiper = new Swiper(".services-swiper", {
+		slidesPerView: 3,
+		spaceBetween: 40,
+		navigation: {
+			nextEl: ".swiper-button__next",
+			prevEl: ".swiper-button__prev",
+		},
+		loop: true,
+		speed: 800
+	});
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
 	content: ["./**/*.{html,js}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				"Oswald": ['Oswald', 'sans-serif'],
+				"Jost": ["Jost", "sans-serif"],
+			},
+			fontSize: {
+				"2xxl": "25px",
+			},
+			colors: {
+				"black": {
+					"DEFAULT": "#0E0D12",
+					"50": "#0e0d13",
+					"60": 'rgba(0,0,0,0.6)'
+				},
+				"red": "#FF002D"
+			},
+			backgroundImage: {
+				"services-bg": "url('../images/services-section-bg.png')",
+				"hero-bg": "url('../images/banner.png')",
+				"patter": "url('../images/pattern-bg.webp')",
+			}
+		},
 	},
 	plugins: [],
 }
