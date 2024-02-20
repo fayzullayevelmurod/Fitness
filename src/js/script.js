b1a4b4f56bfe163cc8353a19549ff9496c4fc622
QobilovSardor <note47297@gmail.com>
2024-02-20 12:35:16 +0500

    tarif section finished

--- src/js/script.js
+++ src/js/script.js
@@ -9,4 +9,15 @@ window.addEventListener('DOMContentLoaded', () => {
 		loop: true,
 		speed: 800
 	});
+
+	var swiper = new Swiper(".comments-swiper", {
+		slidesPerView: 3,
+		spaceBetween: 40,
+		navigation: {
+			nextEl: ".swiper-button__next",
+			prevEl: ".swiper-button__prev",
+		},
+		loop: true,
+		speed: 800
+	});
 })
